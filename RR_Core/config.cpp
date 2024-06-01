fa594cb98f4f65bcd0d073ab1f1001dc3bd2886b
some-evil-kitty <106408180+some-evil-kitty@users.noreply.github.com>
2024-06-01 14:32:54 -0400

    Gitignore + Core

---
+++ RR_Core/config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches
+{
+    class RR_Core
+    {
+        author = "Jenna + Azura";
+		requiredAddons[]=
+		{
+
+		};
+		requiredVersion=0.1;
+		units[]=
+		{
+		};
+		weapons[]={};
+    };
+};

953fb0b7cc7e45c026dc00fd16f6db42804e5a19
some-evil-kitty <106408180+some-evil-kitty@users.noreply.github.com>
2024-07-02 11:06:37 -0400

    Should work now

--- RR_Persistence/config.cpp
+++ RR_Persistence/config.cpp
@@ -11,7 +11,7 @@ class CfgPatches
 		requiredVersion=0.1;
 		units[]=
 		{
-			"RR_Persistence_SaveLoadouts"
+			"RR_Persistence_Saveloadouts_F"
 		};
 		weapons[]={};
     };
@@ -67,42 +67,14 @@ class CfgVehicles
 	class RR_Persistence_Saveloadouts_F : Module_F
 	{
 		// Standard object definitions:
-		scope = 2;										// Editor visibility; 2 will show it in the menu, 1 will hide it.
+		scope = 1;										// Editor visibility; 2 will show it in the menu, 1 will hide it.
 		displayName = "Save All Loadouts";				// Name displayed in the menu
-		category = "Effects";
+		category = "Persistence";
 		scopecurator = 2;
 		function = "RR_Persistence_fnc_SaveAllLoadouts";	// Name of function triggered once conditions are met
 		functionPriority = 1;				// Execution priority, modules with lower number are executed first. 0 is used when the attribute is undefined
 		isGlobal = 0;						// 0 for server only execution, 1 for global execution, 2 for persistent global execution
-		isTriggerActivated = 1;				// 1 for module waiting until all synced triggers are activated
-		isDisposable = 1;					// 1 if modules is to be disabled once it is activated (i.e. repeated trigger activation will not work)
 		is3DEN = 0;							// 1 to run init function in Eden Editor as well
-		curatorCanAttach = 1;				// 1 to allow Zeus to attach the module to an entity
 
-		// 3DEN Attributes Menu Options
-		canSetArea = 1;						// Allows for setting the area values in the Attributes menu in 3DEN
-		canSetAreaShape = 1;				// Allows for setting "Rectangle" or "Ellipse" in Attributes menu in 3DEN
-		canSetAreaHeight = 1;				// Allows for setting height or Z value in Attributes menu in 3DEN
-		class AttributeValues
-		{
-			// This section allows you to set the default values for the attributes menu in 3DEN
-			size3[] = { 100, 100, -1 };		// 3D size (x-axis radius, y-axis radius, z-axis radius)
-			isRectangle = 0;				// Sets if the default shape should be a rectangle or ellipse
-		};
-
-		// Module attributes (uses https://community.bistudio.com/wiki/Eden_Editor:_Configuring_Attributes#Entity_Specific):
-		class Attributes : AttributesBase
-		{
-			// Arguments shared by specific module type (have to be mentioned in order to be present):
-
-			class ModuleDescription : ModuleDescription {}; // Module description should be shown last
-		};
-
-		// Module description (must inherit from base class, otherwise pre-defined entities won't be available)
-		class ModuleDescription : ModuleDescription
-		{
-			description = "Saves all loadouts";	// Short description, will be formatted as structured text
-			sync[] = { };				// Array of synced entities (can contain base classes)
-		};
 	};
 };
